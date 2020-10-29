cf7f2df5927b325581198d0986534c8343ea546b
lss <3106969644@qq.com>
2020-10-29 09:25:48 +0800

    更改mater里的内容，实验冲突

--- src/main/java/com/lss/springboot/Application.java
+++ src/main/java/com/lss/springboot/Application.java
@@ -8,9 +8,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @EnableAdminServer    //启用Spring Boot Admin 组件功能，进行应用的监控，显示监控信息
 public class Application {
     public static void main(String[] args) {
-        System.out.println("hahahaahha");
-        System.out.println("123");
-        System.out.println("567");
+        System.out.println("实验冲突");
         SpringApplication.run(Application.class,args);
     }
 }
